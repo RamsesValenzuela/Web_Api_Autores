426f53f9c2fa310d2539fc5155fea222c486ac63
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-27 01:20:57 -0700

    Create Libro.cs
    book entity created

---
+++ Entidades/Libro.cs
@@ -0,0 +1,17 @@
+﻿namespace Web_Api_Autores.Entidades
+{
+    public class Libro
+    {
+
+        public Libro() { 
+        }
+
+        public int Id { get; set; }
+        
+        public string Titulo { get; set; }
+
+        public int AutorId { get; set; }
+
+        public Autor Autor { get; set; }
+    }
+}
