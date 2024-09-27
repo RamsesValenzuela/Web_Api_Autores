63383c11a837bbbcce8e7ea004ae87b67ebeeae8
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-26 22:22:03 -0700

    Create AutoresController.cs
    The Author Controller was created, where all the endpoints related to authors will be managed

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Api_Autores.Entidades;
+
+namespace Web_Api_Autores.Controllers
+{
+
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        public AutoresController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() { 
+                new Autor() {Id =  1, Nombre = "Jose"},
+                new Autor() {Id = 2, Nombre = "Gustavo" }
+            };
+        }
+        
+    }
+}
