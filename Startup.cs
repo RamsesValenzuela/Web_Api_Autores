73ef2650d6dc8158475d1bebd03fc2d4f1648cea
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-26 22:04:45 -0700

    Create Startup.cs
    The starup class was created where the configuration middleware will be hosted, separating it from the program.cs class to have a more organized project.

---
+++ Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace Web_Api_Autores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configurate(IApplicationBuilder app,  IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
