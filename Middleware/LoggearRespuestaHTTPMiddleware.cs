b6aef4d11c1c36c849507ffe531dfb4f89564abd
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-10-02 16:28:42 -0700

    middleware implementation review
    Implemented a middleware following the c# documentation, using it to display the output information with a HTTPContext, bringing with it all the data that would be displayed to the user before displaying it.

---
+++ Middleware/LoggearRespuestaHTTPMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Web_Api_Autores.Middleware
+{
+    public class LoggearRespuestaHTTPMiddleware
+    {
+        private readonly RequestDelegate siguiente;
+        private readonly ILogger<LoggearRespuestaHTTPMiddleware> logger;
+
+        public LoggearRespuestaHTTPMiddleware(RequestDelegate siguiente, ILogger<LoggearRespuestaHTTPMiddleware>logger)
+        {
+            this.siguiente = siguiente;
+            this.logger = logger;
+        }
+
+        //invoke or InvokeAsync debe de retornar un task y como parametro un http context 
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+                using (var ms = new MemoryStream())
+                {
+                    var cuerpoOriginalResp = context.Response.Body;
+                    context.Response.Body = ms;
+
+                    await siguiente(context);
+
+                    ms.Seek(0, SeekOrigin.Begin);
+                    string respuesta = new StreamReader(ms).ReadToEnd();
+                    ms.Seek(0, SeekOrigin.Begin);
+                    await ms.CopyToAsync(cuerpoOriginalResp);
+                    context.Response.Body = cuerpoOriginalResp;
+
+                    logger.LogInformation(respuesta);
+                };
+            
+        }
+    }
+}
--- Servicios/IServicio.cs
+++ Servicios/IServicio.cs
@@ -2,17 +2,30 @@
 {
     public interface IServicio
     {
+        Guid ObtenerScoped();
+        Guid ObtenerSingleton();
+        Guid ObtenerTransient();
         void RealizarTarea();
     }
 
     public class ServicioA : IServicio
     {
         private readonly ILogger<ServicioA> logger;
+        private readonly ServicioTransient servicioTransient;
+        private readonly ServicioScoped servicioScoped;
+        private readonly ServicioSingleton servicioSingleton;
 
-        public ServicioA(ILogger<ServicioA> logger)
+        public ServicioA(ILogger<ServicioA> logger, ServicioTransient servicioTransient, ServicioScoped servicioScoped, ServicioSingleton servicioSingleton)
         {
             this.logger = logger;
+            this.servicioTransient = servicioTransient;
+            this.servicioScoped = servicioScoped;
+            this.servicioSingleton = servicioSingleton;
         }
+
+        public Guid ObtenerTransient() { return servicioTransient.guid; }
+        public Guid ObtenerScoped() { return servicioScoped.guid; }
+        public Guid ObtenerSingleton() { return servicioSingleton.guid; }
         public void RealizarTarea()
         {
             throw new NotImplementedException();
@@ -21,9 +34,39 @@
 
     public class ServicioB : IServicio
     {
+        public Guid ObtenerScoped()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid ObtenerSingleton()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid ObtenerTransient()
+        {
+            throw new NotImplementedException();
+        }
+
         public void RealizarTarea()
         {
             throw new NotImplementedException();
         }
     }
+
+    public class ServicioTransient
+    {
+        public Guid guid = Guid.NewGuid();
+    }
+    public class ServicioScoped
+    {
+        public Guid guid = Guid.NewGuid();
+    }
+    public class ServicioSingleton
+    {
+        public Guid guid = Guid.NewGuid();
+    }
+
+
 }
