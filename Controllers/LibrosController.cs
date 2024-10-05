001f0a801e2768f31994c612c63e0bd515921a34
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-10-04 18:14:38 -0700

    Update LibrosController.cs

--- Controllers/LibrosController.cs
+++ Controllers/LibrosController.cs
@@ -16,25 +16,25 @@ namespace Web_Api_Autores.Controllers
             this.context = context;
         }
 
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<Libro>> Get(int id)
-        {
-            return await context.Libros.Include(x=> x.Autor).FirstOrDefaultAsync(x => x.Id == id);
-        }
+        //[HttpGet("{id:int}")]
+        //public async Task<ActionResult<Libro>> Get(int id)
+        //{
+        //    return await context.Libros.Include(x=> x.Autor).FirstOrDefaultAsync(x => x.Id == id);
+        //}
 
-        [HttpPost]
-        public async Task<ActionResult> Post(Libro libro)
-        {
-            var existeAutor = await context.Autores.AnyAsync(x => x.Id == libro.AutorId);
+        //[HttpPost]
+        //public async Task<ActionResult> Post(Libro libro)
+        //{
+        //    var existeAutor = await context.Autores.AnyAsync(x => x.Id == libro.AutorId);
 
-            if (!existeAutor)
-            {
-                return BadRequest($"No existe el autor del id: {libro.AutorId}");
-            }
+        //    if (!existeAutor)
+        //    {
+        //        return BadRequest($"No existe el autor del id: {libro.AutorId}");
+        //    }
 
-            context.Add(libro);
-            await context.SaveChangesAsync();
-            return Ok();
-        }
+        //    context.Add(libro);
+        //    await context.SaveChangesAsync();
+        //    return Ok();
+        //}
     }   
 }
