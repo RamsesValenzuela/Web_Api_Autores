0b4444ff8d35cfb1533dff713d66fbdffc590334
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-10-04 18:15:11 -0700

    Create AutorCreacionDTO.cs
    Creation of the DTO Class for the author model

---
+++ DTO/AutorCreacionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Web_Api_Autores.Validaciones;
+
+namespace Web_Api_Autores.DTO
+{
+    public class AutorCreacionDTO
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+
+
+    }
+}
