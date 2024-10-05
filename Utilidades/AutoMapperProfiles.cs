d4e274d5cba541cfa2fdeed04a97b5c527f84161
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-10-04 18:17:09 -0700

    Create AutoMapperProfiles.cs
    automapper configuration class, where the mapper is assigned to the author model

---
+++ Utilidades/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Web_Api_Autores.DTO;
+using Web_Api_Autores.Entidades;
+
+namespace Web_Api_Autores.Utilidades
+{
+    public class AutoMapperProfiles : Profile
+    {
+         public AutoMapperProfiles()
+        {
+            CreateMap<AutorCreacionDTO, Autor>();
+        }
+    }
+}
