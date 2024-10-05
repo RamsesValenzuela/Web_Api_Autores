87abc5bb7a3b1e4917fe2d9836638c86734c56f3
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-10-04 18:15:45 -0700

    Updating
    Updating of author and book models, minor changes

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -4,33 +4,14 @@ using Web_Api_Autores.Validaciones;
 
 namespace Web_Api_Autores.Entidades
 {
-    public class Autor : IValidatableObject
+    public class Autor 
     {
 
         
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength:5, ErrorMessage = "El campo {0} no debe de tener mas de {1} caracteres")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
-
-        
-        public List<Libro> Libros { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (!string.IsNullOrEmpty(Nombre))
-            {
-                var primeraLetra = Nombre[0].ToString();
-
-                if(primeraLetra != primeraLetra.ToUpper())
-                {
-                    yield return new ValidationResult("La primera letra debe de ser mayuscula",
-                        new string[] { nameof(Nombre) });
-                }
-            }
-
-            
-
-        }
     }
 }
--- Entidades/Libro.cs
+++ Entidades/Libro.cs
@@ -3,15 +3,13 @@
     public class Libro
     {
 
-        public Libro() { 
+        public Libro()
+        {
         }
 
         public int Id { get; set; }
-        
+
         public string Titulo { get; set; }
 
-        public int AutorId { get; set; }
-
-        public Autor Autor { get; set; }
     }
 }
