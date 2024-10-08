4e536b3375e6ba50d7a5ec0b990df3554b1e39d8
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-10-08 00:27:47 -0700

    Metadata update

--- Controllers/LibrosController.cs
+++ Controllers/LibrosController.cs
@@ -23,7 +23,12 @@ namespace Web_Api_Autores.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult<LibroDTO>> Get(int id)
         {
-            var libro =  await context.Libros.Include(LibroDB => LibroDB.Comentarios).FirstOrDefaultAsync(x => x.Id == id);
+            var libro =  await context.Libros
+                .Include(LibroDB => LibroDB.AutoresLibros)
+                .ThenInclude(autorLibroDB => autorLibroDB.Autor)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            libro.AutoresLibros = libro.AutoresLibros.OrderBy(x => x.Orden).ToList();
 
             return mapper.Map<LibroDTO>(libro);
         }
@@ -32,7 +37,25 @@ namespace Web_Api_Autores.Controllers
         public async Task<ActionResult> Post(LibroCreacionDTO libroCreacionDTO)
         {
 
+            if (libroCreacionDTO.AutoresIds == null) { return BadRequest("No se puede crear un libro sin autores"); }
+
+
+            var autoresIds = await context.Autores.Where(autorDB => libroCreacionDTO.AutoresIds.Contains(autorDB.Id)).Select(x => x.Id).ToListAsync();
+
+            if (libroCreacionDTO.AutoresIds.Count != autoresIds.Count)
+            {
+                return BadRequest("No existe uno de los autores Enviados");
+            }
+
             var libro = mapper.Map<Libro>(libroCreacionDTO);
+
+            if (libro.AutoresLibros != null) 
+            {
+                for(int i = 0; i < libro.AutoresLibros.Count; i++)
+                {
+                    libro.AutoresLibros[i].Orden = i;
+                }
+            }
 
             context.Add(libro);
             await context.SaveChangesAsync();
--- Utilidades/AutoMapperProfiles.cs
+++ Utilidades/AutoMapperProfiles.cs
@@ -12,13 +12,48 @@ namespace Web_Api_Autores.Utilidades
 
             CreateMap<Autor, AutorDTO>();
 
-            CreateMap<LibroCreacionDTO,  Libro>();
+            CreateMap<LibroCreacionDTO,  Libro>().ForMember(libro => libro.AutoresLibros, opciones => opciones.MapFrom(MapAutoresLibros));
 
-            CreateMap<Libro, LibroDTO>();
+            CreateMap<Libro, LibroDTO>().ForMember(libroDTO => libroDTO.Autores, opciones => opciones.MapFrom(MapLibroDTOAutores));
 
             CreateMap<ComentarioCreacionDTO, Comentario>();
 
             CreateMap<Comentario, ComentarioDTO>();
+
+
+
+        }
+
+        private List<AutoresLibros> MapAutoresLibros(LibroCreacionDTO libroCreacionDTO, Libro libro)
+        {
+            var resultado = new List<AutoresLibros>();
+
+            if (libroCreacionDTO.AutoresIds == null) { return resultado; }
+
+            foreach (var autorId in libroCreacionDTO.AutoresIds)
+            {
+                resultado.Add(new AutoresLibros { AutorId = autorId });
+            }
+
+            return resultado;
+        }
+
+        private List<AutorDTO> MapLibroDTOAutores(Libro libro, LibroDTO libroDTO)
+        {
+            var resultado = new List<AutorDTO>();
+
+            if(libro.AutoresLibros == null) { return resultado; }
+
+            foreach (var autorLibro in libro.AutoresLibros)
+            {
+                resultado.Add(new AutorDTO()
+                {
+                    id = autorLibro.AutorId,
+                    Nombre = autorLibro.Autor.Nombre
+                });
+            }
+
+            return resultado;
         }
     }
 }
