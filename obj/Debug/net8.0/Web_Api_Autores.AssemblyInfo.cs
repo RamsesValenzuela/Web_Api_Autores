b5a70b8239055ca51d054773cc098d7c0ae6f1c0
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-10-16 18:17:13 -0700

    MetaData Changes

--- obj/Debug/net8.0/Web_Api_Autores.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web_Api_Autores.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Api_Autores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0718e8e69dd36cd1c19a22e12394ec942df3cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83c6040dcefbc59cc5f0e0e69eecb939ff9ce1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Api_Autores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Api_Autores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
